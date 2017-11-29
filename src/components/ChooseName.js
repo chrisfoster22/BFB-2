7fee8aa76f7f87766c77e659cfece8d249dad041
Natasha Hakun <natasha.hakun@ey.com>
2017-11-29 14:01:10 -0500

    Add styles to product name input

--- src/components/ChooseName.js
+++ src/components/ChooseName.js
@@ -4,14 +4,34 @@ export default class ChooseName extends Component {
 
     render() {
         return(
-            <div>
-                <div>Choose Name</div>
-                <input type="text" onChange={this.onTextChange.bind((this))} />
+            <div style={styles.chooseNameContainer}>
+                <label style={styles.chooseNameLabel} htmlFor="product">Name Your Product</label>
+                <input style={styles.chooseNameInput} type="text" name="product" id="product" autoComplete="off" onChange={this.onTextChange.bind((this))} />
             </div>
         )
     }
-    
+
     onTextChange(e) {
         this.props.onChoose(e.target.value);
+    }
+}
+
+const styles = {
+    chooseNameContainer: {
+      display: "relative",
+      width: "75%",
+      margin: "0 auto",
+      fontWeight: "300"
+    },
+    chooseNameLabel: {
+      color: "#A1A9AD"
+    },
+    chooseNameInput: {
+        width: "100%",
+        border: "none",
+        borderBottom: "1px solid #3F4345",
+        margin: "10px auto",
+        fontSize: "40px",
+        fontWeight: "300"
     }
 }
