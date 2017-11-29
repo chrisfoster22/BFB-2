6d2a7941677f6dc727afb9d5aafec805ef88f297
Natasha Hakun <natasha.hakun@ey.com>
2017-11-29 14:15:21 -0500

    Style steps text

--- src/components/SidePanel.js
+++ src/components/SidePanel.js
@@ -1,30 +1,33 @@
 import React, { Component } from 'react';
 
 export default class SidePanel extends Component {
-    
-    
+
+
     render() {
         let steps = ["Recipient Type", "Focus Area", "Product Type", "Scents", "Oils", "Butters", "Boosters"]
         let currentStep = this.props.current - 1
-        
+
         let stepDivs = steps.map((step, i) => {
             return(
-                <div style={ i === currentStep ? styles.currentStep : styles.step } key={i}>{step}</div>    
-            ) 
+                <div style={ i === currentStep ? {...styles.step, ...styles.currentStep} : styles.step} key={i}>{step}</div>
+            )
         })
-        
+
         return(
             <div>{stepDivs}</div>
         )
     }
-    
-} 
+
+}
 
 const styles = {
     step: {
-        color: "grey"    
+        fontSize: "18px",
+        fontWeight: "300",
+        color: "#A1A9AD",
+        cursor: "default"
     },
     currentStep: {
-        color: "black"
+        color: "#3F4345"
     }
 }
