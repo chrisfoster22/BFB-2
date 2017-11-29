655dce33f365af2a95258024655358d73bdecec9
Natasha Hakun <hakun.natasha@gmail.com>
2017-11-28 19:57:17 -0500

    Add semantic markup and clean up code

--- src/components/Checkout.js
+++ src/components/Checkout.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import ShopifyBuy from 'shopify-buy'; 
+import ShopifyBuy from 'shopify-buy';
 
 var shopClient = ShopifyBuy.buildClient({
   accessToken: '63e6175cca54c2980bc0cfd7e731a6e7',
@@ -8,56 +8,42 @@ var shopClient = ShopifyBuy.buildClient({
 });
 
 export default class Checkout extends Component {
-    
+
     constructor(props) {
         super(props);
         this.state = this.props.selection;
         this.getProduct = this.getProduct.bind(this)
     }
-    
+
     componentWillMount() {
         this.getProduct(this.props.selection);
     }
-    
+
     render() {
         return(
             <div>{this.state.product.name}</div>
         )
     }
-    
+
     getProduct(selection) {
         shopClient.fetchProduct(selection.product.productId)
         .then((product) => {
-            console.log(product);
             var variant = product.variants[0];
             var quantity = 1;
             var checkoutURL;
             var note = this.formatNote(selection);
-            console.log(note);
-            
+
             // checkoutURL = variant.checkoutUrl(quantity);
             // window.open(("https://www.bestfriendbeauty.org/cart/add?id=" + variant.id));
             window.open(("https://www.bestfriendbeauty.org/cart/add?id=" + variant.id + "&note=" + note));
         })
         .catch(function (e) {
-        console.log(e);
+            console.log(e);
         });
     }
-    
+
     formatNote(selection) {
       return `scents: ${selection.scents.join(', ')}%0Abutters: ${selection.butters.join(', ')}%0Aoils: ${selection.oils.join(', ')}%0Aboosters: ${selection.boosters.join(', ')}`
     }
-    
+
 }
-
-
-
-
-
-
-
-
-
-
-
-
