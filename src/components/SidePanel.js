1f56673b5ad4d345ebef313177e3018fcdc64840
Natasha Hakun <natasha.hakun@ey.com>
2017-11-30 11:57:42 -0500

    Create side panel scroll and fade animations

--- src/components/SidePanel.js
+++ src/components/SidePanel.js
@@ -36,14 +36,16 @@ export default class SidePanel extends Component {
                     return (
                         <div key={i} style={styles.multiChoice}>
                             <div style={{...styles.multiChoiceColor, backgroundColor: this.colors[multiChoice]}}> </div>
-                            <span>{multiChoice}</span>
+                            <span style={styles.multiChoiceText}>{multiChoice}</span>
                         </div>
                     )
                 })
             }
             return(
                 <div key={i} style={styles.stepContainer}>
-                    <div style={styles.step}><span style={{...styles.icon, backgroundImage: `url(${step.icon}`}}></span>{step.text}</div>
+                    <div style={styles.step}>
+                        <span style={{...styles.icon, backgroundImage: `url(${step.icon}`}}></span>{step.text}
+                    </div>
                     <div style={styles.selected}>{selected}</div>
                 </div>
             )
@@ -81,20 +83,33 @@ const styles = {
         paddingLeft: 40,
         fontSize: 20,
         fontWeight: 300,
-        textTransform: "capitalize"
+        textTransform: "capitalize",
+        opacity: 1,
+        WebkitAnimation: "fadeIn 0.5s",
+        MozAnimation: "fadeIn 0.5s",
+        animation: "fadeIn 0.5s"
     },
     multiChoice: {
         display: "inline-block",
         minWidth: 100,
         marginRight: "5%",
-        fontSize: 14,
-        transition: "all 1s"
+        fontSize: 14
     },
     multiChoiceColor: {
+        width: "100%",
         height: 9,
         borderRadius: 100,
         marginTop: 5,
-        marginBottom: 5
+        marginBottom: 5,
+        WebkitAnimation: "scrollIn 0.5s",
+        MozAnimation: "scrollIn 0.5s",
+        animation: "scrollIn 0.5s"
+    },
+    multiChoiceText: {
+        opacity: 1,
+        WebkitAnimation: "fadeIn 0.5s",
+        MozAnimation: "fadeIn 0.5s",
+        animation: "fadeIn 0.5s"
     },
     icon: {
         height: 30,
